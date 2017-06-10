8194bc4aaacb6e5d59481d410ce41a8253abc89f
Fabio <fabio.carmo@computacao.ufla.br>
2017-06-10 18:08:49 -0300

    atualizando foto e criando footer

--- js/iniciaComponentes.js
+++ js/iniciaComponentes.js
@@ -4,3 +4,10 @@ $(document).ready(function () { $(".button-collapse").sideNav(); })
 
 // Iniciando o menu expandível
 $(document).ready(function () { $('.collapsible').collapsible(); });
+
+// Selecionando botão pelo ID e passando função que será executada ao clicar
+// $(document).ready(function () {
+//     $('#botaoAmarelo').click(function() {
+//         alert('tey');
+//     });
+// });
