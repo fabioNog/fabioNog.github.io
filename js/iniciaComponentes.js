7dfdd919920267695c59c2735beedca1794554bd
Fabio <fabio.carmo@computacao.ufla.br>
2017-06-10 17:17:30 -0300

    nova versão

---
+++ js/iniciaComponentes.js
@@ -0,0 +1,6 @@
+
+// Iniciando a NavBar
+$(document).ready(function () { $(".button-collapse").sideNav(); })
+
+// Iniciando o menu expandível
+$(document).ready(function () { $('.collapsible').collapsible(); });
